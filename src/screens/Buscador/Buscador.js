ac889c975b93e767c3d2d031c1800f7f792e2c6a
martumoroni <80982224+martumoroni@users.noreply.github.com>
2022-11-14 10:05:34 -0300

    casi listo buscador buscador.js

--- src/screens/Buscador/Buscador.js
+++ src/screens/Buscador/Buscador.js
@@ -1,7 +1,9 @@
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet, TextInput, FlatList, TextInput } from 'react-native'
 import React, { Component } from 'react'
 import {db, auth} from  '../../firebase/config'
 import Posts from '../Posts/Posts'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSearch} from '@fortawesome/free-solid-svg-icons'
 
 class Buscador extends Component {
     constructor(props) {
@@ -33,4 +35,60 @@ filter() {
     })
 }
 
+render() {
+    return (
+        <View style = {StyleSheet.background}>
+
+            <TextInput
+            style={StyleSheet.buscar}
+            onChangeText={(buscarText)=>
+            
+                this.setState({buscar:buscarText})}
+            
+            placeholder="buscar"
+            keyboardType='email-adress'>
+
+            </TextInput>
+
+            <TouchableOpacity onPress={()=> this.filter()}>
+
+               <FontAwesomeIcon icon={faSearch}>
+                  <Text> Buscar </Text>
+               </FontAwesomeIcon>
+
+            </TouchableOpacity>
+
+            <FlatList
+
+                data={this.state.posts}
+                keyExtractor={(post) => post.id}
+                renderItem={({ item }) => 
+                <Posts postData={item} />}
+
+            ></FlatList>
+
+        </View>
+    )
 }
+}
+
+const styles = StyleSheet.create({
+    background:{
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center'
+    },
+
+    buscar:{
+
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius: 4,
+        borderWidth: 1,
+        borderStyle: 'solid',
+        borderColor: "#23a456"
+    }
+})
+
+export default Buscador
